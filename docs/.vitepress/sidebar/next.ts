73c44ec574b704aff1626206020b05b8e5fc6431
coderfrank <frank0507w@gmail.com>
2025-11-29 20:49:40 +0800

    平行路由

--- docs/.vitepress/sidebar/next.ts
+++ docs/.vitepress/sidebar/next.ts
@@ -10,6 +10,7 @@ const sidebar_network: DefaultTheme.SidebarItem[] = [
 			{ text: "App Router", link: `${URL_next}/AppRouter` },
 			{ text: "路由导航", link: `${URL_next}/路由导航` },
 			{ text: "动态路由", link: `${URL_next}/动态路由` },
+			{ text: "平行路由", link: `${URL_next}/平行路由` },
 		],
 	},
 ];
